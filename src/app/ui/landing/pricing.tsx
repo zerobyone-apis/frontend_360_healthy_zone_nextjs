76ea00ffb92d1953dca40fceea61a0dcdd051e0c
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-01-11 20:12:01 -0300

    Quito comilla flotante

--- src/app/ui/landing/pricing.tsx
+++ src/app/ui/landing/pricing.tsx
@@ -90,7 +90,7 @@ export function Pricing() {
             <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                 <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Choose the best plans</h2>
-                    <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">Choose a plan that's right far your fitness life. Simple pricing & No hidden charges.</p>
+                    <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">Choose a plan thats right far your fitness life. Simple pricing & No hidden charges.</p>
                 </div>
                 <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
 
