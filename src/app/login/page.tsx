6a451091b3611b71ac327bc82529f6a8f8ffafb8
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-02-03 14:47:24 -0300

    Agrego funcionalidad de login y ruteo a singup

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,23 @@
+'use client'
+import { login } from "@/actions/loginActions";
 import Logo from "@/app/ui/svgs/logo-360-healthy-zone.svg";
+import Link from "next/link";
+import Cookies from "js-cookie";
+import { useState } from "react";
 
 export default function Page() {
+
+    const [userdata, setUserData] = useState({ email: "", password: "" });
+
+    async function handleLoginForm() {
+        let resp = await login(userdata);
+        if (resp) {
+            Cookies.set("token", resp, {
+                expires: 7,
+            });
+        }
+    }
+
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -12,14 +29,18 @@ export default function Page() {
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
                             Welcome back!
                         </h1>
-                        <form className="space-y-4 md:space-y-6" action="#">
+                        <form className="space-y-4 md:space-y-6">
                             <div>
                                 <label className="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
-                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@domain.com" required />
+                                <input type="email" name="email" id="email"
+                                    value={userdata.email} onChange={(e) => setUserData({ ...userdata, email: e.target.value })}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@domain.com" required />
                             </div>
                             <div>
                                 <label className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required />
+                                <input type="password" name="password" id="password" placeholder="••••••••"
+                                    value={userdata.password} onChange={(e) => setUserData({ ...userdata, password: e.target.value })}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required />
                             </div>
                             <div className="flex items-center justify-between">
                                 <div className="flex items-start">
@@ -32,9 +53,9 @@ export default function Page() {
                                 </div>
                                 <a href="#" className="text-sm font-medium text-jungle-green-600 hover:underline">Forgot password?</a>
                             </div>
-                            <button type="submit" className="w-full text-white bg-jungle-green-600 hover:bg-jungle-green-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+                            <button type="button" onClick={handleLoginForm} className="w-full text-white bg-jungle-green-600 hover:bg-jungle-green-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
                             <p className="text-sm font-light text-gray-500 ">
-                                Don’t have an account yet? <a href="#" className="font-medium text-jungle-green-600 hover:underline ">Sign up</a>
+                                Don’t have an account yet? <Link href="/signup" className="font-medium text-jungle-green-600 hover:underline ">Sign up</Link>
                             </p>
                         </form>
                     </div>
