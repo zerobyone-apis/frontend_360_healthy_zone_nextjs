ada80e9fcaedc49c5c2df7a16adcbb288a8c7745
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-02-03 14:37:00 -0300

    Método de login

---
+++ src/actions/loginActions.ts
@@ -0,0 +1,25 @@
+"use server";
+
+type login = {
+	email: string;
+	password: string;
+};
+
+export const login = async ({ email, password }: login) => {
+	try {
+		const resp = await fetch("http://localhost:8080/v1.0/user/login", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				email,
+				password,
+			}),
+		});
+
+		console.log(resp.headers.get("Authorization"));
+
+		return resp.headers.get("Authorization") || "";
+	} catch (error) {
+		return console.log(error);
+	}
+};
