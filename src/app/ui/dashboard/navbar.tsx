5ab768e6ce48f3962b31f9ce44d914327b314e3a
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-02-11 02:20:18 -0300

    dropdown para usuario

--- src/app/ui/dashboard/navbar.tsx
+++ src/app/ui/dashboard/navbar.tsx
@@ -1,6 +1,13 @@
+
+import useOnClickOutside from "@/hooks/useOnClickOutside";
+import Cookies from "js-cookie";
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useRef, useState } from "react";
 
 export function Navbar() {
+    const [showProfileDrop, setShowProfileDrop] = useState(false);
     return (
         <nav className="w-full h-[70px] p-3  justify-between content-center flex">
             <form >
@@ -14,10 +21,37 @@ export function Navbar() {
                     <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-200  " placeholder="Search chats, diets, ingredients, etc..." required />
                 </div>
             </form>
-
-            <div className="border-2 p-1 border-jungle-green-400 rounded-full w-10 h-10">
+            <div className="border-2 p-1 border-jungle-green-400 rounded-full w-10 h-10" onClick={() => setShowProfileDrop(!showProfileDrop)}>
                 <Image alt="imagen" width={288} height={288} className="rounded-full" src={"/profile.png"} />
+                <ProfileDropdown show={showProfileDrop} />
             </div>
         </nav >
+    )
+}
+
+function ProfileDropdown({ show = false }) {
+    const router = useRouter();
+    const dropdownRef = useRef(null);
+    const handleClickOutsideFn = () => show = false;
+    useOnClickOutside(dropdownRef, handleClickOutsideFn);
+
+    function handleSignout() {
+        Cookies.remove("token");
+        router.push("/");
+    }
+    if (!show) return null
+    return (
+        <div className="relative inline-block text-left" ref={dropdownRef}>
+            <div className="absolute right-0 z-10 mt-1 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" >
+                <div className="py-1" role="none">
+                    <button className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-0">Account settings</button>
+                    <Link href="/chat" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-1">Support</Link>
+                    <Link href="/license" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-2">License</Link>
+                    <form method="POST" action="#" role="none">
+                        <button className="text-gray-700 block w-full px-4 py-2 text-left text-sm" role="menuitem" id="menu-item-3" onClick={handleSignout}>Sign out</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
