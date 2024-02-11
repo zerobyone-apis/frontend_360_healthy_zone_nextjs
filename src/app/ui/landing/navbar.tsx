d83cece4c3698fc2de4bcce251258815cff77a19
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-02-11 02:20:35 -0300

    logica de usuario logueado o no sobre landing

--- src/app/ui/landing/navbar.tsx
+++ src/app/ui/landing/navbar.tsx
@@ -1,8 +1,19 @@
+'use client'
 import Logo from "@/app/ui/svgs/logo-360-healthy-zone.svg";
 import { Button } from "../button";
-
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
 export function Navbar() {
+	const router = useRouter();
+	const [isLogged, setLogged] = useState(false);
+
+	useEffect(() => {
+		setLogged(Cookies.get("token") ? true : false);
+	})
+
+
 	return (
 		<div className="w-full flex justify-center fixed z-10">
 			<nav className="bg-white shadow-xl flex justify-around items-center px-2 py-2 mt-5  w-[95%] rounded-full border border-slate-200" >
@@ -22,8 +33,18 @@ export function Navbar() {
 					<li><Button className="uppercase">About us</Button></li>
 				</ul>
 				<div className="flex gap-2">
-					<Button className="rounded-full bg-jungle-green-500 hover:bg-jungle-green-300">Sign up</Button>
-					<Button className="rounded-full  border-android-green-500 border hover:bg-android-green-300">Sign in</Button>
+					{
+						isLogged ?
+							<>
+								<Button className="rounded-full bg-jungle-green-500 hover:bg-jungle-green-300" onClick={() => router.push("/dashboard")}>Go Dashboard</Button>
+							</>
+							:
+							<>
+								<Button className="rounded-full bg-jungle-green-500 hover:bg-jungle-green-300" onClick={() => router.push("/sign-up")}>Sign up</Button>
+								<Button className="rounded-full  border-android-green-500 border hover:bg-android-green-300" onClick={() => router.push("/login")}>Sign in</Button>
+							</>
+
+					}
 				</div>
 			</nav>
 		</div>
