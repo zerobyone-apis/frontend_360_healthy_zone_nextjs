3a9b6fd074c5d602c977a1da7c28d3a98a24ab9e
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-02-18 14:18:33 -0300

    sidebar modificado

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
     return (
         <main className="h-screen grid grid-cols-12 bg-jungle-green-50">
-            <div className="col-span-2 flex justify-center h-full items-center">
+            <div className="col-span-2 flex md:justify-start  h-full items-center">
                 <Sidebar></Sidebar>
             </div>
             <div className="flex flex-col col-span-10 h-full overflow-auto">
