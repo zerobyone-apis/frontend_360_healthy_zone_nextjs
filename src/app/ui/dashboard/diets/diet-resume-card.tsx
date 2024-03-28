f0eb79f0cc1940fe191d2d43f78e18306fcb2086
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-03-28 12:08:59 -0300

    refactor: :hammer: change const's name [component/diet-resume-card]

--- src/app/ui/dashboard/diets/diet-resume-card.tsx
+++ src/app/ui/dashboard/diets/diet-resume-card.tsx
@@ -8,23 +8,22 @@ import React from 'react'
 import { Button } from '../../button'
 
 export default function DietResumeCard({ diet }: { diet: DietResponseDTO }) {
-    const prueba = DietResumeCardStyles;
+    const dietObject = DietResumeCardStyles;
     return (
         <Link href={{
-            pathname: "/dashboard/diets/" + diet.diet_id,
-            query: JSON.stringify(diet)
+            pathname: "/dashboard/diets/" + diet.diet_id
         }}
             className={twMerge(clsx("flex-grow border-l-8 rounded-md px-3 py-2 w-full bg-slate-50",
-                prueba[diet.diet_status].card,
+                dietObject[diet.diet_status].card,
             ))}>
             {diet.type}
             <div className=" pt-1 flex flex-col items-start gap-2">
                 <span className='font-thin text-sm'>{diet.description_diet}</span>
                 <div className='flex justify-between w-full'>
-                    <label className={twMerge(clsx("p-1 rounded ", diet.diet_status && prueba[diet.diet_status].label))}>
-                        <p className={twMerge(clsx('font-bold', diet.diet_status && prueba[diet.diet_status].labelText))}>{diet.diet_status}</p>
+                    <label className={twMerge(clsx("p-1 rounded ", diet.diet_status && dietObject[diet.diet_status].label))}>
+                        <p className={twMerge(clsx('font-bold', diet.diet_status && dietObject[diet.diet_status].labelText))}>{diet.diet_status}</p>
                     </label>
-                    <Button className={clsx('p-2 border-2 rounded font-bold', prueba[diet.diet_status].label)} >DETAILS </Button>
+                    <Button className={clsx('p-2 border-2 rounded font-bold', dietObject[diet.diet_status].label)} >DETAILS </Button>
                 </div>
             </div>
 
