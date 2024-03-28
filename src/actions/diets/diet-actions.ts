be7cbb660d1c592488ecb543af17c3b92252ecfe
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-03-28 12:00:58 -0300

    feat: :fire: Mockup data on promise response [diet-actions]

--- src/actions/diets/diet-actions.ts
+++ src/actions/diets/diet-actions.ts
@@ -1,7 +1,22 @@
 "use server";
+import { DietResponseDTO } from "@/interfaces/diets";
 import mockup from "./mockup";
 
 export async function getAllDietsByUserID(userID: number) {
 	// funcion hardcoded
 	return mockup;
+}
+
+export async function getDietById(dietID: number) {
+	// funcion hardcoded
+	let diet: DietResponseDTO | null = await new Promise((resolve, _reject) => {
+		setTimeout(() => {
+			let dietResponse: DietResponseDTO | null =
+				mockup.find(data => data.diet_id === dietID) || null;
+			return resolve(dietResponse);
+		}, 2000);
+	});
+
+	console.log(diet);
+	return diet;
 }
