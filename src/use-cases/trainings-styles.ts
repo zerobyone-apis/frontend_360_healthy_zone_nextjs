2c0ae9484778d95d386de2d5d1950f19890a53c9
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-03-31 00:16:20 -0300

    feat: :fire: new use-case for training cards [view/resume-trainings]

---
+++ src/use-cases/trainings-styles.ts
@@ -0,0 +1,38 @@
+export const TrainingResumeCardStyles = {
+	COMPLETED: {
+		card: "border-jungle-green-400 bg-jungle-green-100",
+		label: "border-b-4 border-jungle-green-500",
+		labelText: "text-green",
+	},
+	CREATED: {
+		card: "border-android-green-300 bg-gray-50 border-b-2 border-t-2 border-r-2",
+		label: "",
+		labelText: "",
+	},
+	PAUSED: {
+		card: "border-android-green-300 bg-gray-50 border-b-2 border-t-2 border-r-2",
+		label: "",
+		labelText: "",
+	},
+	RESERVED: {
+		card: "border-android-green-300 bg-gray-50 border-b-2 border-t-2 border-r-2",
+		label: "",
+		labelText: "",
+	},
+	"NOT APPLY": {
+		card: "border-red-360-400 bg-red-360-200",
+		label: "border-b-4 border-red-360-500",
+		labelText: "",
+	},
+	"IN PROGRESS": {
+		card: "border-teal-300 bg-teal-100",
+		label: "border-teal-400 border-b-4",
+		labelText: "text-teal-400",
+	},
+	IsNotActive: "bg-slate-300",
+	CANCELED: {
+		card: "border-red-360-400 bg-red-360-100",
+		label: "border-b-4 border-red-500",
+		labelText: "text-red-500",
+	},
+};
