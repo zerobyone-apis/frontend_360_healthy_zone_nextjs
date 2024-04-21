8afcd19cbfdd440996fa745ece08f37b0b0db7c8
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-04-20 22:23:59 -0300

    feat: :fire: signout as server action

---
+++ src/actions/dashboard/signout/index.ts
@@ -0,0 +1,12 @@
+"use server";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function signout() {
+	console.log("ingreso en cookie");
+	let cookieStore = cookies();
+	cookieStore.delete("user");
+	cookieStore.delete("token");
+	console.log("Elimino token y user");
+	redirect("/login");
+}
