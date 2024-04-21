8d1bc60b86dbd0e11d5ab61d4dcb3cd33a25c470
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-04-20 22:56:22 -0300

    refactor: :pencil2: jungle green as background for login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -59,7 +59,7 @@ export default function Page() {
     }
 
     return (
-        <section className="bg-gray-50 dark:bg-gray-900">
+        <section className="bg-jungle-green-50">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                     <Logo className="text-[4rem]" width="none"></Logo>
