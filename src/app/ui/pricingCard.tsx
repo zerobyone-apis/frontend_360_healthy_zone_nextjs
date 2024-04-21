46e2dd9552dc5621c01e54de0542bd478e24b63d
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-04-20 22:37:44 -0300

    refactor: :boat: adding navigation to signup

--- src/app/ui/pricingCard.tsx
+++ src/app/ui/pricingCard.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 interface Feature {
     title: string;
@@ -30,7 +31,7 @@ export function PricingCard({ price, title, description, features }: Props) {
                 }
             </ul>
             <div className='w-full h-full flex items-end justify-center'>
-                <button className="text-white border-jungle-green-500 border bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900 w-full">Get started</button>
+                <Link href={"/singup"}><button className="text-jungle-green-500 border-jungle-green-500 border bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-bold rounded-lg text-sm px-5 py-2.5 text-center w-full">Get started</button></Link>
             </div>
         </div>
     )
