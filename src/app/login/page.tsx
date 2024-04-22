f902921a96a0d0cbf7880a3146e702257d37433d
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-04-21 23:29:55 -0300

    refactor: :hammer: mock user disabled

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,42 +12,42 @@ export default function Page() {
     const [userdata, setUserData] = useState({ email: "", password: "" });
 
     async function handleLoginForm() {
-        if (userdata.email === "test@test.com" && userdata.password === "password") {
-            Cookies.set("token", Crypto.encrypt("UnaPruebaDeTokenSinSentido"), {
-                expires: 7
-            });
-            Cookies.set("user",
-                Crypto.encrypt(JSON.stringify({
-                    "user": {
-                        "userId": "1",
-                        "email": "gaston.nicolas.morales.olivera@gmail.com",
-                        "username": "Gaston.Morales",
-                        "roles": "CLIENT",
-                        "isAdmin": false
-                    },
-                    "client": {
-                        "id": 1,
-                        "first_name": "Gaston",
-                        "last_name": "Morales",
-                        "phone": "5986393429239",
-                        "email": "gaston.nicolas.morales.olivera@gmail.com",
-                        "city": "",
-                        "country": "UY",
-                        "description": null,
-                        "profile_picture": "",
-                        "address": null,
-                        "subscription": null,
-                        "nutritionist_id": null,
-                        "coach_id": null,
-                        "goals": [],
-                        "is_blocked": false,
-                        "isActive": true
-                    },
-                    "nutritionist": null,
-                    "coach": null
-                })), { expires: 7 })
-            return true;
-        }
+        // if (userdata.email === "test@test.com" && userdata.password === "password") {
+        //     Cookies.set("token", Crypto.encrypt("UnaPruebaDeTokenSinSentido"), {
+        //         expires: 7
+        //     });
+        //     Cookies.set("user",
+        //         Crypto.encrypt(JSON.stringify({
+        //             "user": {
+        //                 "userId": "1",
+        //                 "email": "gaston.nicolas.morales.olivera@gmail.com",
+        //                 "username": "Gaston.Morales",
+        //                 "roles": "CLIENT",
+        //                 "isAdmin": false
+        //             },
+        //             "client": {
+        //                 "id": 1,
+        //                 "first_name": "Gaston",
+        //                 "last_name": "Morales",
+        //                 "phone": "5986393429239",
+        //                 "email": "gaston.nicolas.morales.olivera@gmail.com",
+        //                 "city": "",
+        //                 "country": "UY",
+        //                 "description": null,
+        //                 "profile_picture": "",
+        //                 "address": null,
+        //                 "subscription": null,
+        //                 "nutritionist_id": null,
+        //                 "coach_id": null,
+        //                 "goals": [],
+        //                 "is_blocked": false,
+        //                 "isActive": true
+        //             },
+        //             "nutritionist": null,
+        //             "coach": null
+        //         })), { expires: 7 })
+        //     return true;
+        // }
         let resp = await login(userdata);
         if (resp) {
             // Cookies.set("token", Crypto.encrypt(resp), {
