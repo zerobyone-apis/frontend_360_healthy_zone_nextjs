d7fe7dcb993db0bb5aaf227a47b39f7b7bfcb414
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-04-21 23:12:12 -0300

    fix: :hammer: renaming properties

--- src/app/ui/dashboard/trainings/exercises-timeline.tsx
+++ src/app/ui/dashboard/trainings/exercises-timeline.tsx
@@ -9,11 +9,11 @@ export default function ExercisesTimeline({ exercises }: { exercises: ExerciseDT
             {exercises.map((exercise, index) =>
                 <li className="mb-10 ms-4" key={index}>
                     <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white "></div>
-                    <time className="mb-1 text-sm font-normal leading-none text-gray-400 ">{exercise.seconds ? exercise.seconds + " seconds" : exercise.reps + " reps, " + exercise.sets + " sets"}</time>
+                    <time className="mb-1 text-sm font-normal leading-none text-gray-400 ">{exercise.duration_in_seconds ? exercise.duration_in_seconds + " seconds" : exercise.repetitions + " reps, " + exercise.series + " sets"}</time>
                     <h3 className="text-lg font-semibold text-gray-900 ">{exercise.name}</h3>
-                    <Image src={'/imgs/russian-twist-exercise.jpeg'} alt={''} width={318} height={159}></Image>
-                    <div className='w-full md:w-[45%]'>
-                        <SeeMoreText textClasses={"mb-4 text-base font-normal text-gray-500"}>
+                    <Image src={exercise.gifUrl || ""} alt={''} width={318} height={159}></Image>
+                    <div className='w-full md:w-[50%]'>
+                        <SeeMoreText textClasses={"mb-4 text-base font-normal text-gray-500 whitespace-pre-line"}>
                             {exercise.description || ""}
                         </SeeMoreText>
                     </div>
