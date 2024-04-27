d5bd869beaea1d781cf3012c0cbd412f05cf8e2b
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-04-27 15:11:49 -0300

    feat: :confetti_ball: Congrats view conditional

--- src/app/ui/dashboard/trainings/training-parent.tsx
+++ src/app/ui/dashboard/trainings/training-parent.tsx
@@ -1,11 +1,18 @@
 "use client"
 import { useState } from "react";
 import RestView from "./rest-view";
+import SuccessView from "./success-view";
 import TrainingView from "./training-view";
 
 
 export default function TrainingParent({ exercises }: any) {
   const [currentIndex, setCurrentIndex] = useState(0)
+  console.log("currentIndex", currentIndex);
+
+  if (currentIndex === exercises.length - 1) {
+    {/** Exercises finished */ }
+    return (<SuccessView />)
+  }
 
   if (!exercises[currentIndex].rest) return (
     <TrainingView setCurrentIndex={setCurrentIndex} exercise={exercises[currentIndex]}></TrainingView>
