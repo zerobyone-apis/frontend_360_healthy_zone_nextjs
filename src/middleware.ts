2384863451848bab0e75884f8204dda97303d7fe
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-04-28 11:29:35 -0300

    feat: :lock: Security update > redirect to corresponding dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,56 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { parseJwt } from "./utils/parseJwt";
 
 export function middleware(request: NextRequest) {
-	const currentToken = request.cookies.get("token")?.value || "";
+	const currentToken = request.cookies.get("token")?.value
+		? parseJwt(request.cookies.get("token")?.value || "")
+		: "";
+
+	const Roles: any = {
+		COACH: "/coach/dashboard",
+		CLIENT: "/dashboard",
+		NUTRITIONIST: "/nutritionist/dashboard",
+	};
+
+	const pathname = request.nextUrl.pathname;
+
 	console.log("current token", currentToken);
 	console.log("actual path", request.nextUrl.pathname);
 
-	if (!currentToken && request.nextUrl.pathname.startsWith("/dashboard")) {
+	//Si es una persona intentando acceder al dashboard y no tiene token, lo enviamos al login de regreso.
+	if (
+		!currentToken &&
+		(request.nextUrl.pathname.startsWith("/dashboard") ||
+			request.nextUrl.pathname.startsWith(
+				"/coach/dashboard" ||
+					request.nextUrl.pathname.startsWith("/nutritionist/dashboard")
+			))
+	) {
 		return NextResponse.redirect(new URL("/login", request.url));
 	}
 
+	// Si estamos en login, mandar al usuario a su correspondiente lugar
 	if (currentToken && request.nextUrl.pathname.startsWith("/login")) {
-		return NextResponse.redirect(new URL("/dashboard", request.url));
+		return NextResponse.redirect(
+			new URL(Roles[currentToken.role], request.url)
+		);
 	}
+
+	// Evitaremos el acceso a las diferentes areas si no tienen el acceso a las mismas
+	if (currentToken && pathname !== Roles[currentToken.role])
+		return NextResponse.redirect(
+			new URL(Roles[currentToken.role], request.url)
+		);
+
 	return NextResponse.next();
 }
 
 export const config = {
-	matcher: ["/dashboard/:path*", "/login/:path*"],
+	matcher: [
+		"/dashboard/:path*",
+		"/login/:path*",
+		"/coach/dashboard/:path*",
+		"/nutritionist/dashboard/:path*",
+	],
 };
