8805411f1fca4933c0b1b5f15e8bf6828f6d94dd
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-05-11 12:02:10 -0300

    refactor: :new: new dependencie on tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,7 @@ const config: Config = {
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/use-cases/**/*.{js,ts,jsx,tsx,mdx}",
 		"./node_modules/flowbite/**/*.js",
+		"./node_modules/flowbite-react/lib/**/*.js",
 	],
 	theme: {
 		extend: {
