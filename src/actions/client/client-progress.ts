0e366e171188aad2bbad1bb9bd2116f193fc58d1
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-05-11 11:59:43 -0300

    feat: :new: client progress actions

---
+++ src/actions/client/client-progress.ts
@@ -0,0 +1,33 @@
+"use server";
+import { cookies } from "next/headers";
+
+export async function getClientProgress(clientId: string) {
+	const cookieStore = cookies();
+	const tokenValue = cookieStore.get("token")?.value || "";
+	const token = tokenValue;
+	try {
+		const resp = await fetch(
+			process.env.BASE_PATH + "/v1.0/progress/all/by/client/" + clientId,
+			{
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					"Cache-Control": "no-store",
+					Authorization: token,
+				},
+				next: {
+					revalidate: 10,
+				},
+			}
+		);
+
+		let respi = await resp.json();
+		console.log(respi);
+		return respi;
+	} catch (error) {
+		return {
+			error: true,
+			message: "Error trying to get information, try later.",
+		};
+	}
+}
