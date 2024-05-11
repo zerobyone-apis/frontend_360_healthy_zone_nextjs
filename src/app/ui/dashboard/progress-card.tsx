12f3d3a521450d0cf625d55672ca06c1272eae43
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-05-11 12:06:10 -0300

    refactor: :pencil2: new styled progress card

--- src/app/ui/dashboard/progress-card.tsx
+++ src/app/ui/dashboard/progress-card.tsx
@@ -9,21 +9,22 @@ type Props = {
     percent: number;
     title: string;
     currentProgress: string;
+    icon?: string;
 }
 
-export default function ProgressCard({ target, percent, bcolor, tcolor, currentProgress, title }: Props) {
+export default function ProgressCard({ target, percent, bcolor, tcolor, currentProgress, title, icon }: Props) {
 
     return (
         <div className="h-[275px] flex justify-end flex-col w-full">
-            <div className='w-full h-[237px] max-w-full max-h-full border rounded-3xl shadow-lg flex flex-col justify-around items-center'>
+            <div className='w-full h-[237px] max-w-full max-h-full border rounded-3xl shadow-lg flex flex-col justify-around items-center bg-white'>
                 <div className={clsx(`rounded-3xl w-[92px] h-[83px] relative flex justify-center items-center -top-8`, bcolor)}>
-                    <i className='bx bx-run text-4xl font-bold text-white'></i>
+                    <i className={clsx(icon ? icon : "bx bx-run", 'text-4xl font-bold text-white')}></i>
                 </div>
-                <div>
-                    <h3 className={clsx('text-3xl font-bold text-center', tcolor)}>
+                <div className=''>
+                    <h3 className={clsx('text-2xl font-bold text-center', tcolor)}>
                         {title}
                     </h3>
-                    <h5 className='text-xl font-extralight text-center'>
+                    <h5 className='text-4xl font-bold text-center'>
                         {currentProgress}
                     </h5>
                 </div>
