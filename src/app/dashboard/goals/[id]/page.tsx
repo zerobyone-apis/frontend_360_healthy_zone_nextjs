de8fed9f71418f314b07d24f3e5dac885baa8512
Damian Rodriguez <damianezetiel1@gmail.com>
2024-05-14 18:49:47 -0300

    implement goals into dashboard

---
+++ src/app/dashboard/goals/[id]/page.tsx
@@ -0,0 +1,53 @@
+import { getDietById } from "@/actions/diets"
+import { Button } from "@/app/ui/button";
+import DefaultCard from "@/app/ui/dashboard/default-card";
+import HorizontalTimeline from "@/app/ui/dashboard/horizontal-timeline";
+import { DietResponseDTO } from "@/interfaces/diets";
+import { DietResumeCardStyles } from "@/use-cases/diets-styles";
+import clsx from "clsx";
+
+export default async function Page({ params }: { params: { id: string } }) {
+    const diet: DietResponseDTO | null = await getDietById(Number(params.id));
+
+    return (
+        <>
+            <div className="flex flex-col items-center">
+                <div className="text-center">
+                    <h4 className="text-3xl font-bold text-gray-700 font-sans">{diet?.type}</h4>
+                    <h5 className="text-xl text-gray-600 font-sans">{diet?.description_diet}</h5>
+                </div>
+                <label className={clsx("p-1 rounded ", DietResumeCardStyles[diet?.diet_status || "NOT APPLY"].label)}>
+                    <p className={clsx('font-bold', DietResumeCardStyles[diet?.diet_status || "NOT APPLY"].labelText)}>{diet?.diet_status || "NOT APPLY"}</p>
+                </label>
+                <div className="mt-4 ml-6">
+                    <HorizontalTimeline init_on={diet?.init_on || ""} end_on={diet?.end_on || ""} />
+                </div>
+                <div className="grid grid-cols-2 w-full p-4 gap-2">
+                    <DefaultCard classes="col-span-2 md:col-span-2" titleClasses="text-jungle-green-700 mb-2 text-2xl font-bold tracking-tight" title="Recipes" body={diet?.healthy_recipes || "No content"} />
+                    <DefaultCard classes="col-span-2 md:col-span-1" titleClasses="text-jungle-green-700 mb-2 text-2xl font-bold tracking-tight" title="Shopping list" body={diet?.healthy_shopping_list || "No content"} />
+                    <DefaultCard classes="col-span-2 md:col-span-1" titleClasses="text-jungle-green-700 mb-2 text-2xl font-bold tracking-tight" title="Portion size guide" body={diet?.portion_size_guide || "No content"} />
+                    <DefaultCard classes="col-span-2 md:col-span-1" titleClasses="text-jungle-green-700 mb-2 text-2xl font-bold tracking-tight" title="Balanced Meal Plan" body={diet?.balanced_meal_plan || "No content"} />
+                    <DefaultCard classes="col-span-2 md:col-span-1" titleClasses="text-jungle-green-700 mb-2 text-2xl font-bold tracking-tight" title="Hydratation" body={diet?.hydratation || "No content"} />
+                    <DefaultCard
+                        classes="col-span-2 md:col-span-2 border-blue-300 border-5 bg-blue-100 border-l-8"
+                        titleClasses="text-blue-800 mb-2 text-2xl font-bold tracking-tight"
+                        bodyClasses="text-blue-900 whitespace-pre-line"
+                        title="Tips!"
+                        body={diet?.tips_for_change_eating_habits || "No content"} />
+                    <DefaultCard
+                        classes="col-span-2 md:col-span-2 border-violet-300 border-5 bg-violet-100 border-l-8"
+                        titleClasses="text-violet-800 mb-2 text-2xl font-bold tracking-tight"
+                        bodyClasses="text-violet-900 whitespace-pre-line" title="Food Education" body={diet?.food_education || "No content"} />
+                    <DefaultCard classes="col-span-2 md:col-span-2 border-teal-300 border-5 bg-teal-100 border-l-8"
+                        titleClasses="text-teal-800 mb-2 text-2xl font-bold tracking-tight"
+                        bodyClasses="text-teal-900 whitespace-pre-line" title="Nutrition information" body={diet?.nutrition_information || "No content"} />
+                </div>
+            </div>
+            <div className="w-full flex justify-center">
+                <Button className="bg-jungle-green-500 rounded text-white hover:bg-jungle-green-400 fixed md:bottom-4 bottom-20 font-sans font-bold gap-1">
+                    <i className='bx bxs-flag-checkered text-2xl'></i>MARK AS COMPLETED
+                </Button>
+            </div>
+        </>
+    )
+}
---
+++ src/app/dashboard/goals/page.tsx
@@ -0,0 +1,36 @@
+'use client'
+import React from "react";
+import { ClientTrainingGoals } from "@/app/goals/client-training-goals";
+import { ClientDietGoals } from "@/app/goals/client-diet-goals";
+
+enum PAGES {
+  TRAINING = "TRAINING",
+  DIET = "DIET",
+}
+
+enum USER_TYPES {
+  CLIENT = "CLIENT",
+  TRAINER = "TRAINER",
+  DIETIST = "DIETIST",
+}
+
+export default function page() {
+
+  // CHANGE THIS FOR GET USER TYPE
+  const user = {
+    userType: USER_TYPES.CLIENT,
+    goalType: PAGES.TRAINING
+  }
+
+  if (user.userType === USER_TYPES.CLIENT) {
+    if (user.goalType === PAGES.TRAINING) {
+      return <ClientTrainingGoals />
+    }
+    if (user.goalType === PAGES.DIET) {
+      return <ClientDietGoals />
+    }
+    return null;
+    // TODO define a default value in case of error/empty value
+  }
+};
+
