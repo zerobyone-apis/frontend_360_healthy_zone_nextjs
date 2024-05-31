a5f7a7dd460bd6e31a97ebecd6d91fa82847556d
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-05-31 10:30:14 -0300

    feat: :pencil: adding redirect on task list

--- src/app/ui/dashboard/tasks-list.tsx
+++ src/app/ui/dashboard/tasks-list.tsx
@@ -1,8 +1,10 @@
-'use client'
+import Link from "next/link";
+
 type task = {
     title: string;
     subtitle: string;
     id: string;
+    redirect: string;
 }
 type Props = {
     tasks: task[];
@@ -14,13 +16,14 @@ export function TasksList({ tasks }: Props) {
             {
                 tasks.map(task => {
                     return (
-                        <div key={task.id} className={'w-full min-h-[40px] bg-white rounded-xl p-2 flex gap-2 items-center'}>
-                            <div className="bg-jungle-green-500 rounded-full w-10 h-10"></div>
-                            <div className="">
-                                <h3 className='text-base font-semibold'>{task.title}</h3>
-                                <h5 className='text-sm font-light'>{task.subtitle}</h5>
+                        <Link href={task.redirect}>
+                            <div key={task.id} className={'w-full min-h-[40px] bg-white rounded-xl p-2 flex gap-2 items-center'}>
+                                <div className="w-full">
+                                    <h3 className='text-base font-semibold'>{task.title}</h3>
+                                    <h5 className='text-sm font-light truncate'>{task.subtitle}</h5>
+                                </div>
                             </div>
-                        </div>
+                        </Link>
                     )
                 })
             }
