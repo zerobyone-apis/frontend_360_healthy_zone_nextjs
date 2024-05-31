fd9efa19beca5356a427017195c0c3af7c3e430f
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-05-31 10:45:08 -0300

    refactor: :pencil: adding user card on customer page

--- src/app/coach/dashboard/customers/page.tsx
+++ src/app/coach/dashboard/customers/page.tsx
@@ -1,14 +1,22 @@
 import { getDashboardStats } from '@/actions/coach/dashboard';
+import CreateTrainingModal from '@/app/ui/coach/create-training-modal';
+import { UserCard } from '@/app/ui/dashboard/user-card';
 import React from 'react'
 
 type Props = {}
 
 export default async function Page({ }: Props) {
     const stats = await getDashboardStats();
+    console.log(stats);
 
     return (
-        <div>
-            {JSON.stringify(stats.full_assignments)}
-        </div>
+        <>
+            <div className='flex flex-col gap-2'>
+                {stats.full_assignments.map((stat: any) =>
+                    <UserCard user={stat.client} key={stat.id} />
+                )}
+            </div>
+            <CreateTrainingModal />
+        </>
     )
 }
