46de054313c4df5668edffe67f703a0b70ac3eb3
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2024-05-31 10:41:31 -0300

    refactor: :hammer: adding props to tasks card

--- src/app/ui/dashboard/tasks-card.tsx
+++ src/app/ui/dashboard/tasks-card.tsx
@@ -1,28 +1,49 @@
 import { TasksList } from "./tasks-list"
 
-type Props = {}
 
-export function TasksCard({ }: Props) {
-    const tasks = [{
-        title: "Call with nutritionist",
-        subtitle: "10 AM CST",
-        id: "2dmai2ad"
-    },
-    {
-        title: "Follow coach instructions",
-        subtitle: "Exercise post launch time",
-        id: "2dmai2a2d"
-    },
-    {
-        title: "Go, Go, Go!",
-        subtitle: "Run for 2km and mark it done when you finish",
-        id: "2dmai212313ad"
-    }]
+type task = {
+    title: string;
+    subtitle: string;
+    id: string;
+    redirect: string;
+}
+
+type Props = { tasks?: task[]; }
+
+export function TasksCard({ tasks }: Props) {
+    // const tasks = [{
+    //     title: "Active training",
+    //     subtitle: "This is a current training description...",
+    //     id: "2dmai2ad",
+    //     redirect: "/"
+    // },
+    // {
+    //     title: "Active diet",
+    //     subtitle: "99 Por ejemplo, reemplazar los snacks poco saludables por opciones más nutritivas como frutas frescas, nueces o yogur griego, y reducir gradualmente el consumo de alimentos procesados",
+    //     id: "2dmai2a2d",
+    //     redirect: "/"
+    // },
+    // {
+    //     title: "Current Goal",
+    //     subtitle: "Reducir un 8% del fatiga al hacer ejercicios en los próximos seis meses de manera sostenible y saludable.",
+    //     id: "2dmai212313ad",
+    //     redirect: "/"
+    // }]
+
+    if (!tasks || tasks.length === 0) {
+        return (
+            <div className="w-full  bg-jungle-green-500 flex flex-col justify-center p-4 rounded-3xl shadow gap-2">
+                <div className="w-[90%] h-full flex justify-center text-center items-center gap-2">
+                    <i className="bx bx-ghost text-xl text-white"></i>
+                    <h5 className="text-xl text-white">No Activities Pending</h5>
+                </div>
+            </div>)
+    }
+
     return (
         <div className="w-full  bg-jungle-green-500 flex flex-col justify-center p-4 rounded-3xl shadow gap-2">
-            <div className="w-full p-2 flex justify-between">
-                <p className="font-light text-white">Pending tasks: <span className="font-bold">{tasks.length}</span></p>
-                <p className="text-white">View All...</p>
+            <div className="w-full p-2 flex justify-start">
+                <p className="font-light text-white">Activities: <span className="font-bold">{tasks.length}</span></p>
             </div>
             <div className="w-[90%] h-full overflow-auto">
                 <TasksList tasks={tasks} />
