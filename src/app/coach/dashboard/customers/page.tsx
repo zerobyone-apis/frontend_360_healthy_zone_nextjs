72011ef31d8b538a7468ee78c72a274f890eea4d
gaston.nicolas.morales.olivera <gaston.nicolas.morales.olivera@gmail.com>
2024-06-10 10:41:32 +0000

    feat: :new: adding user card on my customers page

--- src/app/coach/dashboard/customers/page.tsx
+++ src/app/coach/dashboard/customers/page.tsx
@@ -1,14 +1,19 @@
 import { getDashboardStats } from '@/actions/coach/dashboard';
+import CustomerCard from '@/app/ui/coach/customer-card';
 import React from 'react'
 
 type Props = {}
 
 export default async function Page({ }: Props) {
     const stats = await getDashboardStats();
+    console.log(stats);
 
+    if (!stats) return null;
     return (
-        <div>
-            {JSON.stringify(stats.full_assignments)}
+        <div className="gap-3">
+            {stats.full_assignments.map((assigned: any) => {
+                return (<CustomerCard client={assigned.client}/>)
+            })}
         </div>
     )
 }
