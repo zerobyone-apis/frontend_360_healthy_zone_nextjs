70e3db3eeaa455fa38d9b6f0b362e9368fc7edfb
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-06-16 19:21:25 -0300

    feat: :new: get Goals server action

---
+++ src/actions/goals/get-goals.tsx
@@ -0,0 +1,34 @@
+"use server";
+import { GoalResponseDTO } from "@/interfaces/goals";
+import { cookies } from "next/headers";
+
+//based on the bellow code, create a function that will return the goals
+export async function getGoals(
+	clientID: number
+): Promise<GoalResponseDTO[] | Array<never>> {
+	const cookieStore = cookies();
+	const tokenValue = cookieStore.get("token")?.value || "";
+	const token = tokenValue;
+
+	try {
+		const resp = await fetch(
+			process.env.BASE_PATH +
+				"/v1.0/professional/goal/all/by/client/" +
+				clientID,
+			{
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: token,
+				},
+				body: null,
+				cache: "no-store",
+			}
+		);
+		let body = await resp.json();
+		return body;
+	} catch (error) {
+		console.log(error);
+		return [];
+	}
+}
