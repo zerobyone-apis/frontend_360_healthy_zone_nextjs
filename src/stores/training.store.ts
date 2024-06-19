185df1bf125b5f4b5c500b414da664e92d08f2d3
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-06-18 21:34:20 -0300

    feat: :new: create training store

---
+++ src/stores/training.store.ts
@@ -0,0 +1,31 @@
+import { create } from 'zustand'
+
+export const trainingStore = create((set) => ({
+  dialogs: {
+    newTraining: false,
+    selectTraining: false
+  },
+  training: {
+    coach_id: 0,
+    goal_id: 0,
+    type: "",
+    training_status: "",
+    coach_plans: "",
+    description_training: "",
+    frequency: "",
+    init_on: "",
+    amount_of_days: 0,
+    daily_training_days: [],
+  },
+  currentGoal: null,
+  setOpenNewTraining: (isOpen: boolean) => set((state: any) => ({ dialogs: { ...state.dialogs, newTraining: isOpen } })),
+  setOpenSelectTraining: (isOpen: boolean) => set((state: any) => ({ dialogs: { ...state.dialogs, selectTraining: isOpen } })),
+  setTraining: (training: any) => set((state: any)=> ({ training: {...state.training, ...training} })),
+  setCurrentGoal: (goal: any) => set({ currentGoal: goal, training: { ...goal }}),
+  removeTraining: () => set({ training: { coach_id: 0, goal_id: 0, type: "", training_status: "", coach_plans: "", description_training: "", frequency: "", init_on: "", amount_of_days: 0, daily_training_days: [] } }),
+  clearCurrentGoal: () => set({ currentGoal: null }),
+  
+  // increasePopulation: () => set((state: { bears: number }) => ({ bears: state.bears + 1 })),
+  // removeAllBears: () => set({ bears: 0 }),
+  // updateBears: (newBears: any) => set({ bears: newBears }),
+}))
