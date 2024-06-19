f3a4594668829ee59637014588c9d5139d0e972a
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-06-18 21:34:48 -0300

    fix: :pencil: design fix

--- src/app/ui/dashboard/navbar-mobile.tsx
+++ src/app/ui/dashboard/navbar-mobile.tsx
@@ -1,34 +1,46 @@
-'use client'
+"use client";
 import clsx from "clsx";
 import Link from "next/link";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 
 export function NavbarMobile({ list }: { list: ListItemInterface[] }) {
-
-    return (
-        <nav className='max-h-[60px] w-[95%] rounded-full mb-2 p-2 relative bottom-0 bg-jungle-green-500 '>
-            <div className="h-full w-full flex justify-center">
-                <ul className=" pl-3 font-bold flex justify-center flex-row items-center gap-2 w-full text-center">
-                    {list.map((li, index) => <ListItem {...li} key={index}></ListItem>)}
-                </ul>
-            </div>
-        </nav>
-    )
+	return (
+		<nav className="max-h-[60px] w-[95%] rounded-full mb-2 p-2 fixed bottom-0 bg-jungle-green-500 ">
+			<div className="h-full w-full flex justify-center">
+				<ul className=" pl-3 font-bold flex justify-center flex-row items-center gap-2 w-full text-center">
+					{list.map((li, index) => (
+						<ListItem {...li} key={index}></ListItem>
+					))}
+				</ul>
+			</div>
+		</nav>
+	);
 }
 
 interface ListItemInterface {
-    icon: any;
-    title: string;
-    redirect: string;
+	icon: any;
+	title: string;
+	redirect: string;
 }
 function ListItem({ icon, title, redirect = "" }: ListItemInterface): any {
-    const pathname = usePathname();
+	const pathname = usePathname();
 
-    return (
-        <div className="w-full">
-            <Link key={redirect} href={redirect}>
-                <li className={clsx('w-full flex gap-2 p-2 align center content-start text-center hover:text-black active:text-jungle-green-500 hover:bg-white active:bg-white rounded-full transition-colors justify-center', pathname == redirect ? 'bg-white text-jungle-green-500' : "text-white")} aria-description={title}> <i className={icon}></i> </li>
-            </Link >
-        </div>
-    )
+	return (
+		<div className="w-full">
+			<Link key={redirect} href={redirect}>
+				<li
+					className={clsx(
+						"w-full flex gap-2 p-2 align center content-start text-center hover:text-black active:text-jungle-green-500 hover:bg-white active:bg-white rounded-full transition-colors justify-center",
+						pathname == redirect
+							? "bg-white text-jungle-green-500"
+							: "text-white"
+					)}
+					aria-description={title}
+				>
+					{" "}
+					<i className={icon}></i>{" "}
+				</li>
+			</Link>
+		</div>
+	);
 }
