b87f661db31e0aa79a93809efdc8589583694e4f
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-06-21 10:04:21 -0300

    feat: :new: create training server action

---
+++ src/actions/trainings/create-training.ts
@@ -0,0 +1,44 @@
+"use server"
+import { Training } from "@/interfaces/trainings";
+import { cookies } from "next/headers";
+
+export async function createTraining({clientID, training}: {clientID: string, training: Training}) {
+    const cookieStore = cookies();
+    const tokenValue = cookieStore.get("token")?.value || "";
+    const token = tokenValue;
+    const user = JSON.parse(
+        cookieStore.get("user")?.value || "{}"
+    );
+    const coach_id = user.coach.id;
+
+    //formatting the training object
+    training.coach_id = coach_id;
+    training.coach_plans = training.coach_plans.replaceAll(" ", "_");
+    training.type = training.type.replaceAll(" ", "_");
+
+    console.log("Creating training for client: ", clientID);
+    console.log("Training: ", training);
+    try {
+        const resp = await fetch(process.env.BASE_PATH + "/v1.0/training/create/client/" + clientID , {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token,
+            },
+            body: JSON.stringify(training),
+            cache: "no-store",
+        });
+        let body = await resp.json();
+        console.log(resp.status);
+        if(resp.status !== 200) {
+            throw new Error(body.message);
+        }
+        
+        console.log(body);
+
+        return body;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
