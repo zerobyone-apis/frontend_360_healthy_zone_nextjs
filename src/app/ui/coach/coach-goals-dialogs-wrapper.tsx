521f4670b2cdcc3dc40f7d7b794c6f62c34ec9f7
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-06-23 20:25:35 -0300

    feat: :new: dialogs wrapper for goals

---
+++ src/app/ui/coach/coach-goals-dialogs-wrapper.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import NewGoalDialog from "../new-goal-dialog";
+import ConfirmationDialog from "../confirmation-dialog";
+import { deactivateGoal } from "@/actions/goals/deactivate-goal";
+import { toast } from "react-toastify";
+
+export default function DialogsWrapper() {
+	const params = useSearchParams();
+	const router = useRouter();
+	const goalId = params.get("goal_id");
+	const confirmDelete = () => {
+		console.log("confirm delete ", goalId);
+		deactivateGoal(Number(goalId))
+			.then(() => {
+				router.push("/coach/dashboard/goals");
+				toast.success("Goal deleted successfully");
+				router.refresh();
+			})
+			.catch(() => {
+				toast.error("Error deleting goal");
+			});
+	};
+
+	return (
+		<>
+			{params.get("new-goal") && <NewGoalDialog />}
+			{params.get("confirm-delete") && (
+				<ConfirmationDialog
+					canClose={true}
+					confirmAction={confirmDelete}
+					cancelAction={() => router.push("/coach/dashboard/goals")}
+					handleClose={() => router.push("/coach/dashboard/goals")}
+					title={"Are you sure you want to delete this goal?"}
+				/>
+			)}
+		</>
+	);
+}
