b41f18f875639d9d88ae6cded499e4f61e2aa8df
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-06-24 09:20:53 -0300

    feat: :new: server action template
    This helps to copying and pasting faster

---
+++ src/actions/template/index.ts
@@ -0,0 +1,40 @@
+"use server"
+
+import { cookies } from "next/headers";
+
+export async function fetchTemplate() {
+    const cookieStore = cookies();
+
+    //getting the token from the cookie
+    const tokenValue = cookieStore.get("token")?.value || "";
+    const token = tokenValue;
+
+    //getting the user from the cookie
+    const user = JSON.parse(
+        cookieStore.get("user")?.value || "{}"
+    );
+
+    try {
+        const resp = await fetch(process.env.BASE_PATH + "/this/would/be/your/path/" /* + user.client.id or some param */, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token,
+            },
+            body: /* body here if required */ null,
+            cache: "no-store",
+        });
+        let body = await resp.json();
+        console.log(resp.status);
+        if(resp.status !== 200) {
+            throw new Error(body.message);
+        }
+        
+        console.log(body);
+
+        return body;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
