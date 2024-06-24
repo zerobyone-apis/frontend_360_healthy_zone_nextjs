6cd90af1d487591372bf90335a01a0a2b25372c9
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-06-23 22:35:47 -0300

    feat: :pencil: useOnClickOutside doc

--- src/hooks/useOnClickOutside.tsx
+++ src/hooks/useOnClickOutside.tsx
@@ -1,32 +1,40 @@
-import { RefObject, useEffect } from 'react';
+import { RefObject, useEffect } from "react";
 
 type AnyEvent = MouseEvent | TouchEvent;
 
+/**
+ * Custom hook that allows detecting clicks outside of a specified element.
+ *
+ * @template T - The type of the element to be referenced.
+ * @param {RefObject<T>} ref - The reference to the element to be monitored for clicks outside.
+ * @param {(event: AnyEvent) => void} handler - The callback function to be executed when a click outside the element occurs.
+ * @param {RefObject<T>} [noRef] - An optional reference to another element that should not trigger the callback when clicked.
+ */
 function useOnClickOutside<T extends HTMLElement = HTMLElement>(
-    ref: RefObject<T>,
-    handler: (event: AnyEvent) => void,
-    noRef?: RefObject<T>,
+	ref: RefObject<T>,
+	handler: (event: AnyEvent) => void,
+	noRef?: RefObject<T>
 ) {
-    useEffect(() => {
-        const listener = (event: AnyEvent) => {
-            const el = ref?.current;
-            const noEl = noRef?.current;
-            if (
-                !el
-                || el.contains(event.target as Node)
-                || (noEl && noEl.contains(event.target as Node))
-            ) {
-                return;
-            }
-            handler(event);
-        };
-        document.addEventListener('mousedown', listener);
-        document.addEventListener('touchstart', listener);
-        return () => {
-            document.removeEventListener('mousedown', listener);
-            document.removeEventListener('touchstart', listener);
-        };
-    }, [ref, handler]);
+	useEffect(() => {
+		const listener = (event: AnyEvent) => {
+			const el = ref?.current;
+			const noEl = noRef?.current;
+			if (
+				!el ||
+				el.contains(event.target as Node) ||
+				(noEl && noEl.contains(event.target as Node))
+			) {
+				return;
+			}
+			handler(event);
+		};
+		document.addEventListener("mousedown", listener);
+		document.addEventListener("touchstart", listener);
+		return () => {
+			document.removeEventListener("mousedown", listener);
+			document.removeEventListener("touchstart", listener);
+		};
+	}, [ref, handler]);
 }
 
 export default useOnClickOutside;
