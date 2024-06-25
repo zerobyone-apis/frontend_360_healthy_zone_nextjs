5c9ef503302d96268cf5da83e72832a3dcb253f1
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-06-24 22:36:45 -0300

    refactor: :hammer: get exercises from db

--- src/actions/trainings/index.ts
+++ src/actions/trainings/index.ts
@@ -3,6 +3,11 @@ import { TrainingResponseDto } from "@/interfaces/trainings";
 import mockup, { exercises_from_api, mock_daily_train } from "./mockup";
 import { cookies } from "next/headers";
 
+/**
+ * Retrieves all trainings for the current user.
+ * 
+ * @returns A Promise that resolves to an array of training objects.
+ */
 export async function getAllTrainings() {
 	const cookieStore = cookies();
     const tokenValue = cookieStore.get("token")?.value || "";
@@ -34,28 +39,122 @@ export async function getAllTrainings() {
     }
 }
 
-export async function getTrainingsID(trainingID: number) {
-	// funcion hardcoded
-	let training: TrainingResponseDto | null = await new Promise(
-		(resolve, _reject) => {
-			setTimeout(() => {
-				let trainingResponse: TrainingResponseDto | null =
-					mockup.find(data => data.training_id === trainingID) || null;
-				return resolve(trainingResponse);
-			}, 2000);
-		}
-	);
 
-	console.log(training);
-	return training;
+
+export async function getTrainingsID(trainingID: number) {
+	//TODO
+//delete this const and use the function from the server
+const daily_training_days = [
+    {
+      "number_training_day": 1,
+      "total_training_days": 2,
+      "selected_exercises": [
+    {
+        "name": "3/4 sit-up",
+        "type": "ABS",
+        "description": "Lie flat on your back with your knees bent and feet flat on the ground.\nPlace your hands behind your head with your elbows pointing outwards.\nEngaging your abs, slowly lift your upper body off the ground, curling forward until your torso is at a 45-degree angle.\nPause for a moment at the top, then slowly lower your upper body back down to the starting position.\nRepeat for the desired number of repetitions.",
+        "series": "",
+        "repetitions": "",
+        "duration_in_seconds": 20,
+        "rest_in_seconds": 30,
+        "difficulty": 1,
+        "url_image": "https://storage.googleapis.com/avance-clientes/exercises-imgs/ec3bb682-8f8a-4671-a22f-85ed9d1847d0.gif",
+        "is_completed": false
+    },
+    {
+        "name": "45° side bend",
+        "type": "ABS",
+        "description": "Stand with your feet shoulder-width apart and your arms extended straight down by your sides.\nKeeping your back straight and your core engaged, slowly bend your torso to one side, lowering your hand towards your knee.\nPause for a moment at the bottom, then slowly return to the starting position.\nRepeat on the other side.\nContinue alternating sides for the desired number of repetitions.",
+        "series": "",
+        "repetitions": "",
+        "duration_in_seconds": 25,
+        "rest_in_seconds": 30,
+        "difficulty": 1,
+        "url_image": "https://storage.googleapis.com/avance-clientes/exercises-imgs/da3a4cc8-1baf-42ba-bdd7-2028db1ffecc.gif",
+        "is_completed": false
+    },
+    {
+        "name": "air bike",
+        "type": "ABS",
+        "description": "Lie flat on your back with your hands placed behind your head.\nLift your legs off the ground and bend your knees at a 90-degree angle.\nBring your right elbow towards your left knee while simultaneously straightening your right leg.\nReturn to the starting position and repeat the movement on the opposite side, bringing your left elbow towards your right knee while straightening your left leg.\nContinue alternating sides in a pedaling motion for the desired number of repetitions.",
+        "series": 3,
+        "repetitions": 5,
+        "duration_in_seconds": "",
+        "rest_in_seconds": 30,
+        "difficulty": 1,
+        "url_image": "https://storage.googleapis.com/avance-clientes/exercises-imgs/04eecbad-f8aa-4cb4-b74b-13e0e07913b5.gif",
+        "is_completed": false
+    }
+]
+    },
+    {
+      "number_training_day": 2,
+      "total_training_days": 2,
+      "selected_exercises": [
+    {
+        "name": "3/4 sit-up",
+        "type": "ABS",
+        "description": "Lie flat on your back with your knees bent and feet flat on the ground.\nPlace your hands behind your head with your elbows pointing outwards.\nEngaging your abs, slowly lift your upper body off the ground, curling forward until your torso is at a 45-degree angle.\nPause for a moment at the top, then slowly lower your upper body back down to the starting position.\nRepeat for the desired number of repetitions.",
+        "series": "",
+        "repetitions": "",
+        "duration_in_seconds": 20,
+        "rest_in_seconds": 30,
+        "difficulty": 1,
+        "url_image": "https://storage.googleapis.com//avance-clientes/exercises-imgs/ec3bb682-8f8a-4671-a22f-85ed9d1847d0.gif",
+        "is_completed": false
+    },
+    {
+        "name": "45° side bend",
+        "type": "ABS",
+        "description": "Stand with your feet shoulder-width apart and your arms extended straight down by your sides.\nKeeping your back straight and your core engaged, slowly bend your torso to one side, lowering your hand towards your knee.\nPause for a moment at the bottom, then slowly return to the starting position.\nRepeat on the other side.\nContinue alternating sides for the desired number of repetitions.",
+        "series": "",
+        "repetitions": "",
+        "duration_in_seconds": 25,
+        "rest_in_seconds": 30,
+        "difficulty": 1,
+        "url_image": "https://storage.googleapis.com//avance-clientes/exercises-imgs/da3a4cc8-1baf-42ba-bdd7-2028db1ffecc.gif",
+        "is_completed": false
+    },
+    {
+        "name": "air bike",
+        "type": "ABS",
+        "description": "Lie flat on your back with your hands placed behind your head.\nLift your legs off the ground and bend your knees at a 90-degree angle.\nBring your right elbow towards your left knee while simultaneously straightening your right leg.\nReturn to the starting position and repeat the movement on the opposite side, bringing your left elbow towards your right knee while straightening your left leg.\nContinue alternating sides in a pedaling motion for the desired number of repetitions.",
+        "series": 3,
+        "repetitions": 5,
+        "duration_in_seconds": "",
+        "rest_in_seconds": 30,
+        "difficulty": 1,
+        "url_image": "https://storage.googleapis.com//avance-clientes/exercises-imgs/04eecbad-f8aa-4cb4-b74b-13e0e07913b5.gif",
+        "is_completed": false
+    }
+]
+    }
+  ]
+
+	// funcion hardcoded
+	
+	try{
+		let trainings = await getAllTrainings();
+		let training = trainings.find((training: any) => training.training_id == trainingID);
+
+		training.daily_training_days = daily_training_days;
+		return training;
+	}catch(error){
+		console.log(error);
+		return null;
+	}
+
+	
 }
 
-export async function getExerciseDay(daily_train_id: string) {
-	console.log("DailyTrain ID", daily_train_id);
+export async function getExerciseDay(number_day: string, training_id: string) {
 
 	// Funcion hardcoded...
 	let training: any = await new Promise(async (resolve, _reject) => {
-		let trainingResponse = await exercises_from_api();
+		let trainingResponse = await getTrainingsID(Number(training_id));
+		let dailyTraining = trainingResponse.daily_training_days.find(
+			(day: any) => day.number_training_day == Number(number_day)
+		);
 		//fragmentando ejercicios y descansos:
 		let exercisesAndRests: any = [
 			/**
@@ -67,22 +166,21 @@ export async function getExerciseDay(daily_train_id: string) {
 			},
 		];
 
-		trainingResponse.forEach((exerciseData: any, index: number) => {
+		dailyTraining.selected_exercises.forEach((exerciseData: any, index: number) => {
 			let rest = {
 				rest: true,
 				rest_in_seconds: exerciseData.rest_in_seconds,
 			};
 			let exercise = { ...exerciseData, rest: false };
 			exercisesAndRests.push(exercise);
-			if (index !== trainingResponse.length - 1) exercisesAndRests.push(rest);
+			if (index !== dailyTraining.length - 1) exercisesAndRests.push(rest);
 		});
 		return resolve({
-			training: trainingResponse,
+			training: dailyTraining,
 			exercises: exercisesAndRests,
 		});
 	});
 
-	console.log(training);
 	return training;
 }
 
