f17f63f6638bafe3469df16929cfe855b7ba42a5
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-06-25 22:33:38 -0300

    feat: :+1: difficulty and dtos

--- src/app/dashboard/trainings/[id]/page.tsx
+++ src/app/dashboard/trainings/[id]/page.tsx
@@ -2,25 +2,35 @@ import { getTrainingsID } from "@/actions/trainings";
 import { Button } from "@/app/ui/button";
 import ExercisesTimeline from "@/app/ui/dashboard/trainings/exercises-timeline";
 import TrainingDifficultyCard from "@/app/ui/dashboard/trainings/training-difficulty-card";
+import { Training } from "@/interfaces/trainings";
 import Link from "next/link";
 
 export default async function Page({ params }: { params: { id: string } }) {
-	const training = await getTrainingsID(Number(params.id));
-	console.log(training);
+	const training: Training = await getTrainingsID(Number(params.id));
+
+	const difficulty =
+		training.dailyTrainingDays[0].selected_exercises.reduce(
+			(acc, exercise) => acc + exercise.difficulty,
+			0
+		) / training.dailyTrainingDays[0].selected_exercises.length;
+
 	const currentPath = "/dashboard/trainings/" + params.id;
 	return (
 		<>
 			<div className="grid grid-cols-4 gap-3">
 				<div className="col-span-4">
 					<h3 className="font-sans font-bold text-center text-xl">
-						{training?.type}
+						{training?.type.replaceAll("_", " ")}
 					</h3>
 					<h5 className="font-sans font-thin text-center">
 						{training?.description_training}
 					</h5>
 				</div>
 				<div className="col-span-4 md:col-span-1 gap-4 flex flex-col">
-					<TrainingDifficultyCard days_remaining={20} difficulty={3} />
+					<TrainingDifficultyCard
+						days_remaining={training.amount_of_days}
+						difficulty={difficulty}
+					/>
 					<Link href={currentPath + "/in-progress/2"}>
 						<Button className="hidden bg-teal-500 rounded text-white hover:bg-jungle-green-400 font-sans font-bold gap-1 md:flex justify-center">
 							<i className="bx bx-play text-2xl"></i>START TRAINING
@@ -30,13 +40,13 @@ export default async function Page({ params }: { params: { id: string } }) {
 
 				<div className="col-span-4 md:col-span-3">
 					<ExercisesTimeline
-						exercises={training?.daily_training_days[0].selected_exercises}
+						exercises={training?.dailyTrainingDays[0].selected_exercises}
 					/>
 				</div>
 				<div className="hidden col-span-1 md:block"></div>
 			</div>
 			<div className="w-full flex justify-center">
-				<Link href={currentPath + "/in-progress/2"}>
+				<Link href={currentPath + "/in-progress/1"}>
 					<Button className="md:hidden bg-teal-500 rounded text-white hover:bg-jungle-green-400 fixed md:bottom-4 bottom-20 font-sans font-bold gap-1">
 						<i className="bx bx-play text-2xl"></i>START TRAINING
 					</Button>
