447b4b25c79475b6af296f4af3a887a6473852d8
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-07-04 22:30:32 -0300

    removing unnecessary comments

--- src/stores/training.store.ts
+++ src/stores/training.store.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand'
 
-export const trainingStore = create((set) => ({
+export const trainingStore = create((set, get) => ({
   dialogs: {
     newTraining: false,
     selectTraining: false
@@ -24,8 +24,4 @@ export const trainingStore = create((set) => ({
   setCurrentGoal: (goal: any) => set((state: any)=>({ currentGoal: goal, training: { ...state.training, goal_id: goal.id}})),
   resetTraining: () => set({ training: { coach_id: 0, goal_id: 0, type: "", training_status: null, coach_plans: "", description_training: "", frequency: "", init_on: null, amount_of_days: 0, daily_training_days: [] } }),
   clearCurrentGoal: () => set({ currentGoal: null }),
-  
-  // increasePopulation: () => set((state: { bears: number }) => ({ bears: state.bears + 1 })),
-  // removeAllBears: () => set({ bears: 0 }),
-  // updateBears: (newBears: any) => set({ bears: newBears }),
 }))
