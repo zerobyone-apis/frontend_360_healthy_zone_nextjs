0e32b38e662b0bd64defe11a9ff17bc9202f7f1a
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-07-08 18:10:22 -0300

    feat: :man: header for customers page

--- src/app/coach/dashboard/customers/page.tsx
+++ src/app/coach/dashboard/customers/page.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { getDashboardStats } from "@/actions/coach/dashboard";
 import CustomerCard from "@/app/ui/coach/customer-card";
-import NewTrainingDialogParent from "@/app/ui/coach/new-training-parent.dialog";
+import Link from "next/link";
 
 export default function Page() {
 	const [stats, setStats] = useState<any>(null);
@@ -14,11 +14,34 @@ export default function Page() {
 
 	if (!stats) return null;
 	return (
-		<div className="gap-3">
-			{stats.full_assignments.map((assigned: any) => {
-				return <CustomerCard key={assigned.id} client={assigned.client} />;
-			})}
-			<NewTrainingDialogParent />
-		</div>
+		<section>
+			<div className="gap-2 flex flex-col md:inline-flex md:flex-row justify-between w-full mb-5 p-5">
+				<h1 className="text-xl text-jungle-green-700 font-bold">Customers</h1>
+				<div className="gap-2 inline-flex">
+					<Link
+						href="/coach/dashboard/goals?new-goal=true"
+						type="button"
+						className="px-3 py-2 text-xs font-medium text-center hover:text-white border border-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+					>
+						<i className="bx bx-plus me-1"></i>
+						New Goal
+					</Link>
+					<Link
+						href="/coach/dashboard/trainings?new-training=true"
+						type="button"
+						className="px-3 py-2 text-xs font-medium text-center hover:text-white border border-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+					>
+						<i className="bx bx-plus me-1"></i>
+						New training
+					</Link>
+				</div>
+			</div>
+
+			<div className="gap-3">
+				{stats.full_assignments.map((assigned: any) => {
+					return <CustomerCard key={assigned.id} client={assigned.client} />;
+				})}
+			</div>
+		</section>
 	);
 }
