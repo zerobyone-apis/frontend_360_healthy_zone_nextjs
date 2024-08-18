11f0a14d24b9423f31af6d9bdc42579e81e6e788
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-08-17 21:32:54 -0300

    feat: :new: diets page

--- src/app/nutritionist/dashboard/diets/page.tsx
+++ src/app/nutritionist/dashboard/diets/page.tsx
@@ -3,14 +3,22 @@ import React, { useEffect, useState } from "react";
 import { getDashboardStats } from "@/actions/nutritionist/dashboard";
 import CustomerCard from "@/app/ui/coach/customer-card";
 import Link from "next/link";
+import DietResumeCard from "@/app/ui/dashboard/diets/diet-resume-card";
 
 export default function Page() {
     const [stats, setStats] = useState<any>(null);
     const [error, setError] = useState<boolean>(false);
+    const [diets, setDiets] = useState<any[]>([])
     useEffect(() => {
         getDashboardStats()
-            .then(data => {
-                setStats(data);
+            .then((data: any) => {
+                setStats((prev: any) => ({ ...prev, ...data }));
+                data.goals_created.forEach((goal: any) => {
+                    const complete_diet_list = goal.diets.map((diet: any) => {
+                        return { ...diet, client_info: { ...goal.client } };
+                    });
+                    setDiets([...diets, ...complete_diet_list]);
+                });
             })
             .catch(e => {
                 console.error(e);
@@ -36,18 +44,10 @@ export default function Page() {
     return (
         <section>
             <div className="gap-2 flex flex-col md:inline-flex md:flex-row justify-between w-full mb-5 p-5">
-                <h1 className="text-xl text-jungle-green-700 font-bold">Customers</h1>
+                <h1 className="text-xl text-jungle-green-700 font-bold">Diets</h1>
                 <div className="gap-2 inline-flex">
                     <Link
-                        href="/coach/dashboard/goals?new-goal=true"
-                        type="button"
-                        className="px-3 py-2 text-xs font-medium text-center hover:text-white border border-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
-                    >
-                        <i className="bx bx-plus me-1"></i>
-                        New Goal
-                    </Link>
-                    <Link
-                        href="/coach/dashboard/trainings?new-training=true"
+                        href="/nutritionist/dashboard/diets?new-diet=true"
                         type="button"
                         className="px-3 py-2 text-xs font-medium text-center hover:text-white border border-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
                     >
@@ -57,10 +57,8 @@ export default function Page() {
                 </div>
             </div>
 
-            <div className="gap-3">
-                {stats.full_assignments.map((assigned: any) => {
-                    return <CustomerCard key={assigned.id} client={assigned.client} />;
-                })}
+            <div className="gap-3 flex flex-col">
+                {diets.map((diet: any, index: number) => <DietResumeCard key={diet.diet_id} diet={diet} index={index} />)}
             </div>
 
         </section>
