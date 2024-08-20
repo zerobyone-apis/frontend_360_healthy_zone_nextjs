3fa23758d29cafaf045eff4ac4bddfaca31617f6
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-08-20 19:35:49 -0300

    layout refactor for all

--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { Navbar } from "../../ui/dashboard/navbar";
-import { Sidebar } from "../../ui/dashboard/sidebar";
-import { NavbarMobile } from "../../ui/dashboard/navbar-mobile";
+import Sidebar from "@/app/ui/sidebar-flowbite";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -31,19 +29,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
 	return (
 		<>
-			<main className="h-screen grid grid-cols-12 bg-jungle-green-50">
-				<div className="col-span-2 md:flex md:justify-start  hidden h-full items-center">
-					<Sidebar list={navbarList} />
-				</div>
-				<div className="flex flex-col col-span-12 md:col-span-10 h-full overflow-auto">
-					<Navbar />
-					<main className=" h-[95%] max-h-full p-3 overflow-auto">
+			<main className=" bg-white">
+				<Sidebar list={navbarList} />
+				<main className="p-4 sm:ml-64">
+					<div className="p-4 mt-14">
 						{children}
-					</main>
-				</div>
-				<div className="md:hidden flex col-span-12 justify-center">
-					<NavbarMobile list={navbarList} />
-				</div>
+					</div>
+				</main>
 			</main>
 			<ToastContainer />
 		</>
