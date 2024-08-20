e9143fdd07310f412e458092760f443cbe2d52c0
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-08-20 19:15:25 -0300

    layout refactor

--- src/app/nutritionist/dashboard/layout.tsx
+++ src/app/nutritionist/dashboard/layout.tsx
@@ -1,8 +1,5 @@
 "use client";
-import { Navbar } from "../../ui/dashboard/navbar";
-// import { Sidebar } from "../../ui/dashboard/sidebar";
 import Sidebar from "@/app/ui/sidebar-flowbite";
-import { NavbarMobile } from "../../ui/dashboard/navbar-mobile";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -38,20 +35,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
 			<main className=" bg-white">
-
 				<Sidebar list={navbarList} />
-				{/* <div className="col-span-2 md:flex md:justify-start  hidden h-full items-center">
-					 <Sidebar list={navbarList} />
-				</div> */}
-
 				<main className="p-4 sm:ml-64">
 					<div className="p-4 mt-14">
 						{children}
 					</div>
 				</main>
-				{/* <div className="md:hidden flex col-span-12 justify-center">
-					<NavbarMobile list={navbarList} />
-				</div> */}
 			</main>
 			<ToastContainer />
 		</>
