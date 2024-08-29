40025956817af7dbcbbea489c3534f5237be000f
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-08-28 21:52:04 -0300

    fix: exercises from local json

--- src/actions/trainings/get-exercises.ts
+++ src/actions/trainings/get-exercises.ts
@@ -1,47 +1,58 @@
-"use server"
-import fs from 'fs/promises';
-import path from 'path';
+"use server";
+import fs from "fs/promises";
+import path from "path";
+import exercisesList from "./exercises.data";
 
-const dataFilePath = path.join(process.cwd(), 'public', 'data', 'exercises.json');
+const dataFilePath = path.join(
+	process.cwd(),
+	"public",
+	"data",
+	"exercises.json"
+);
 type Params = {
-    limit?: number;
-    page?: number;
-    target?: string;
-    name?: string;
+	limit?: number;
+	page?: number;
+	target?: string;
+	name?: string;
+};
+export async function getExercises({
+	limit = 10,
+	page = 1,
+	target = "",
+	name = "",
+}: Params) {
+	try {
+		const exercises = exercisesList;
+
+		const startIndex = (page - 1) * limit;
+		const endIndex = page * limit;
+
+		let filteredExercises = exercises;
+
+		if (target) {
+			filteredExercises = filteredExercises.filter(
+				(exercise: any) => exercise.target === target
+			);
+		}
+
+		if (name) {
+			filteredExercises = filteredExercises.filter((exercise: any) =>
+				exercise.name.toLowerCase().includes(name.toLowerCase())
+			);
+		}
+
+		const paginatedExercises = filteredExercises.slice(startIndex, endIndex);
+
+		return {
+			exercises: paginatedExercises,
+			total: filteredExercises.length,
+			showing: paginatedExercises.length,
+			pages: Math.ceil(filteredExercises.length / limit),
+			page: page,
+			limit: limit,
+		};
+	} catch (error) {
+		console.error("Error reading exercises file:", error);
+		return null;
+	}
 }
-export async function getExercises({ limit = 10, page = 1, target = "", name = "" }: Params) {
-    try {
-        const data = await fs.readFile(dataFilePath, 'utf8');
-        const exercises = JSON.parse(data);
-
-        const startIndex = (page - 1) * limit;
-        const endIndex = page * limit;
-
-        let filteredExercises = exercises;
-
-        if (target) {
-            filteredExercises = filteredExercises.filter((exercise: any) => exercise.target === target);
-        }
-
-        if (name) {
-            filteredExercises = filteredExercises.filter((exercise: any) =>
-                exercise.name.toLowerCase().includes(name.toLowerCase())
-            );
-        }
-
-        const paginatedExercises = filteredExercises.slice(startIndex, endIndex);
-
-        return {
-            exercises: paginatedExercises,
-            total: filteredExercises.length,
-            showing: paginatedExercises.length,
-            pages: Math.ceil(filteredExercises.length / limit),
-            page: page,
-            limit: limit
-        };
-    } catch (error) {
-        console.error('Error reading exercises file:', error);
-        return null;
-    }
-}
-
