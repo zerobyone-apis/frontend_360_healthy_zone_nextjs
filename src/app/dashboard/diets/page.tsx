a893f622f4a1f61e9e2ad30d11376b2090220855
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-08-28 21:04:54 -0300

    get real diets by id

--- src/app/dashboard/diets/page.tsx
+++ src/app/dashboard/diets/page.tsx
@@ -1,23 +1,25 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import DietResumeCard from "@/app/ui/dashboard/diets/diet-resume-card";
-import { getAllDietsByUserID } from "@/actions/diets";
 import Cookies from "js-cookie";
 import { User } from "@/interfaces/user";
 import { DietResponseDTO } from "@/interfaces/diets";
 import DietCounterChart from "@/app/ui/dashboard/counter-chart";
 import Image from "next/image";
+import { getAllDietsByUser } from "@/actions/diets/get-all-diets-by-user";
 
 export default function Page() {
 	const [diets, setDiets] = useState<any[]>([]);
 
 	const user: User = JSON.parse(Cookies.get("user") || "{}");
 	const userId = Number(user.user?.userId);
+	const dietsCompleted: number = diets.filter((diet) => diet.isCompleted === true).length || 0;
 
 	useEffect(() => {
 		async function fetchDiets() {
 			try {
-				const resp = await getAllDietsByUserID(userId);
+				const resp = await getAllDietsByUser();
+				console.log(resp);
 				setDiets(resp);
 			} catch (error) {
 				console.error("Error fetching diets:", error);
@@ -47,7 +49,7 @@ export default function Page() {
 					bg="bg-jungle-green-100"
 					border="border-jungle-green-500"
 					title="✅ COMPLETED ✅"
-					count={diets.filter.length}
+					count={dietsCompleted}
 				/>
 
 				<DietCounterChart
