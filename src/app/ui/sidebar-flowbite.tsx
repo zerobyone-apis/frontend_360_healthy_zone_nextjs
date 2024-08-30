f16a62d211ea5f53bd1060846821415a8c3913a1
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-08-30 02:44:41 -0300

    fix: removing href on logo

--- src/app/ui/sidebar-flowbite.tsx
+++ src/app/ui/sidebar-flowbite.tsx
@@ -28,7 +28,7 @@ export default function Sidebar({ list }: Props) {
                                     <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                                 </svg>
                             </button>
-                            <a href="https://flowbite.com" className="flex ms-2 md:me-24">
+                            <a href="#" className="flex ms-2 md:me-24">
                                 <Logo width={"100%"} height={"20px"} />
                                 <span className="sr-only self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">360 healthy zone</span>
                             </a>
