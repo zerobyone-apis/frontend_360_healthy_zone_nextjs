9a6107e912cd30af91677152ca3138bd5e299e43
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-08-30 00:10:41 -0300

    feat: get diet by id

---
+++ src/actions/diets/get-diet-bt-id.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export async function getDietByID(id: string | number) {
+	const cookieStore = cookies();
+
+	//getting the token from the cookie
+	const tokenValue = cookieStore.get("token")?.value || "";
+	const token = tokenValue;
+
+	//getting the user from the cookie
+	const user = JSON.parse(cookieStore.get("user")?.value || "{}");
+
+	try {
+		const resp = await fetch(process.env.BASE_PATH + "/v1.0/diet/by/id/" + id, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: token,
+			},
+			body: /* body here if required */ null,
+			cache: "no-store",
+		});
+		let body = await resp.json();
+
+		if (!resp.ok) {
+			throw new Error(body.message);
+		}
+
+		return body;
+	} catch (error) {
+		console.log(error);
+		throw new Error("Error trying to get all the diets");
+	}
+}
