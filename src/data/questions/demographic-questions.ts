7a4a670639894ad51b36fa0dba36648504d95ee4
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-09-15 21:23:35 -0300

    feat: :new: questions

---
+++ src/data/questions/demographic-questions.ts
@@ -0,0 +1,50 @@
+import { QuestionDTO } from "@/interfaces/questions";
+
+export default (): QuestionDTO[] => {
+	return [
+		{
+			name: "about_you",
+			type: "fields",
+			title: "About you",
+			subtitle: "This info will help us to understand how can we help you",
+			img: "https://www.voxco.com/wp-content/uploads/2021/04/students-feedback-survey-cvr.jpg",
+			fields: [
+				{
+					id: "age",
+					label: "Age",
+					placeholder: "How old are you?",
+					type: "numeric",
+					icon: "bx bx-x",
+				},
+				{
+					id: "gender",
+					label: "Gender",
+					type: "radio",
+					placeholder: "What is your gender?",
+					options: [
+						"💁‍♂️ Male",
+						"🙋‍♀️ Female",
+						"🤫 I prefer not to say it",
+						"Other",
+					],
+					icon: "",
+				},
+
+				{
+					id: "ocuppation",
+					type: "text",
+					placeholder: "What is your occupation?",
+					label: "Ocuppation",
+					icon: "",
+				},
+				{
+					id: "address",
+					type: "text",
+					placeholder: "Where are you located?",
+					label: "Address",
+					icon: "",
+				},
+			],
+		},
+	];
+};
---
+++ src/data/questions/medical-questions.ts
@@ -0,0 +1,17 @@
+export default [
+	{
+		name: "medical_history",
+		type: "radio-and-other",
+		title: "Do you have any medical conditions we should be aware of?",
+		subtitle: null,
+		img: "https://www.voxco.com/wp-content/uploads/2021/04/students-feedback-survey-cvr.jpg",
+		options: [
+			"None",
+			"Diabetes",
+			"Hypertension",
+			"Heart Disease",
+			"Food Allergies",
+			"Other, please specify",
+		],
+	},
+];
