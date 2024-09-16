3f0b90b6ea4f5ed2190e000d0b5e9ab504eec48a
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-09-15 21:21:46 -0300

    refactor: deprecated custom form

---
+++ src/app/custom-form-DEPRECATED/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { useRouter } from "next/navigation";
+import { SURVEY_PAGES } from "../ui/mind-logger/libs/questions"
+import { Survey } from "../ui/mind-logger/survey"
+
+export default function Page() {
+    const router = useRouter();
+
+    const onSubmit = (values: any) => {
+        console.log(values)
+        router.push("/")
+    }
+    return <Survey pages={SURVEY_PAGES} onSubmit={onSubmit} />
+}
