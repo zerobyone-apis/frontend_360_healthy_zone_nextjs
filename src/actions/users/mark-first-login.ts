fd816aed6193d4f3d6f6883304b76000343361b7
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-09-20 20:48:52 -0300

    feature: :new: mark as first login
    this api is not working, but should be fixed later by the backend.

---
+++ src/actions/users/mark-first-login.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export async function markFirstLogin() {
+	const cookieStore = cookies();
+
+	//getting the token from the cookie
+	const tokenValue = cookieStore.get("token")?.value || "";
+	const token = tokenValue;
+
+	//getting the user from the cookie
+	const user = JSON.parse(cookieStore.get("user")?.value || "{}");
+	const userID = user.user.userId;
+
+	try {
+		const resp = await fetch(
+			process.env.BASE_PATH + "/v1.0/user/mark/" + userID,
+			{
+				method: "PATCH",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: token,
+				},
+				body: /* body here if required */ null,
+				cache: "no-store",
+			}
+		);
+		let body = await resp.json();
+
+		if (!resp.ok) {
+			throw new Error(body.message);
+		}
+
+		return body;
+	} catch (error) {
+		throw new Error("Error trying to mark user first login ");
+	}
+}
