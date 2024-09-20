06ab804bff0bf4805dc76798618be69f510878ab
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-09-20 20:48:21 -0300

    feature: :new: create custom form api

---
+++ src/actions/customForm/create-custom-form.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export async function createCustomForm(formData: object) {
+	const cookieStore = cookies();
+
+	//getting the token from the cookie
+	const tokenValue = cookieStore.get("token")?.value || "";
+	const token = tokenValue;
+
+	//getting the user from the cookie
+	const user = JSON.parse(cookieStore.get("user")?.value || "{}");
+
+	try {
+		const resp = await fetch(
+			process.env.BASE_PATH + "/v1.0/custom-form/create/" + user.client.id,
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: token,
+				},
+				body: JSON.stringify(formData),
+				cache: "no-store",
+			}
+		);
+		let body = await resp.json();
+
+		if (!resp.ok) {
+			throw new Error(body.message);
+		}
+
+		return body;
+	} catch (error) {
+		console.error(error);
+		throw new Error("Error trying to create custom form");
+	}
+}
