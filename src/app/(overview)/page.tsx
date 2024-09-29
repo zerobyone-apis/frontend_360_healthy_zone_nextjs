f36c7791e46e2358b754ea906d3d43da38ab0a22
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-09-28 21:16:34 -0300

    adding new sections

--- src/app/(overview)/page.tsx
+++ src/app/(overview)/page.tsx
@@ -1,13 +1,19 @@
-import { AboutUS } from "../ui/landing/about-us";
+import AboutUs from "../ui/landing/about-us";
+import { Benefits } from "../ui/landing/benefits";
 import { ContactForm } from "../ui/landing/contact-form"
 import { Hero } from "../ui/landing/hero";
+import PreviewSection from "../ui/landing/preview-section";
 import { Pricing } from "../ui/landing/pricing";
+import { Steps } from "../ui/landing/steps";
 
 export default function Page() {
   return (
     <main >
       <Hero />
-      <AboutUS />
+      <AboutUs />
+      <Steps />
+      <PreviewSection />
+      <Benefits />
       <Pricing />
       <ContactForm />
     </main>
