1ed409e84c046323c72cdee78ca893e4e4fe8a06
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-11-18 02:24:19 -0300

    Adding block CTA on users parge

--- src/app/admin/dashboard/users-list/page.tsx
+++ src/app/admin/dashboard/users-list/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { blockUserRequest } from '@/actions/admin/block-user';
 import { getAllProfiles } from '@/actions/profile/get-all-profiles';
-import { Button, ButtonGroup, Spinner, Table, TextInput, Pagination } from 'flowbite-react';
+import { Button, ButtonGroup, Spinner, Table, TextInput, Pagination, Modal } from 'flowbite-react';
 import React, { useEffect, useState } from 'react';
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { toast } from 'react-toastify';
 
 
 type Props = {};
@@ -29,6 +32,7 @@ export default function Page({ }: Props) {
             })
             .finally(() => {
                 setLoading(false);
+                console.log(profiles);
             });
     }, []);
 
@@ -50,6 +54,33 @@ export default function Page({ }: Props) {
         (currentPage - 1) * profilesPerPage,
         currentPage * profilesPerPage
     );
+
+    const [openModal, setOpenModal] = useState<boolean>(false);
+    const [selectedUser, setSelectedUser] = useState<any>();
+
+    const handleCloseModal = () => {
+        setOpenModal(false)
+    }
+
+    const handleConfirmBlock = async (reason: string) => {
+
+        try {
+            await blockUserRequest(selectedUser.user_id, reason);
+            toast.success("User blocked successfully");
+            setSelectedUser(null);
+            setTimeout(() => {
+                window.location.reload();
+            }, 100);
+        } catch (e) {
+            toast.error("Something went wrong, try again");
+        }
+
+    }
+
+    const handleDisableUser = (user: any) => {
+        setSelectedUser(user);
+        setOpenModal(true);
+    }
 
     if (loading) {
         return (
@@ -117,7 +148,7 @@ export default function Page({ }: Props) {
                                         <Button color="gray" size="xs" onClick={() => console.log("Enable")}>
                                             Enable
                                         </Button>
-                                        <Button color="gray" size="xs" onClick={() => console.log("Disable")}>
+                                        <Button color="gray" size="xs" onClick={() => handleDisableUser(profile)}>
                                             Disable
                                         </Button>
                                     </ButtonGroup>
@@ -134,6 +165,39 @@ export default function Page({ }: Props) {
                 onPageChange={setCurrentPage}
                 className="mt-4"
             />
+
+            <BlockUserModal openModal={openModal} handleClose={handleCloseModal} handleConfirm={handleConfirmBlock} />
         </div>
     );
+}
+
+
+type ModalProps = {
+    openModal: boolean;
+    handleClose: () => void;
+    handleConfirm: (reason: string) => void;
+}
+function BlockUserModal({ openModal, handleClose, handleConfirm }: ModalProps) {
+    const [reason, setReason] = useState<string>("");
+
+    return (<Modal show={openModal} size="md" onClose={handleClose} popup>
+        <Modal.Header />
+        <Modal.Body>
+            <div className="text-center">
+                <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+                <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                    Are you sure you want to block this user?
+                </h3>
+                <TextInput placeholder='Reason why...' className='mb-4' value={reason} onChange={(e) => setReason(e.target.value)}></TextInput>
+                <div className="flex justify-center gap-4">
+                    <Button color="failure" onClick={() => handleConfirm(reason)} disabled={!reason}>
+                        {"Yes, I'm sure"}
+                    </Button>
+                    <Button color="gray" onClick={handleClose}>
+                        No, cancel
+                    </Button>
+                </div>
+            </div>
+        </Modal.Body>
+    </Modal>)
 }
