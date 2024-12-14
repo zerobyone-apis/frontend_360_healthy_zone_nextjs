34ce6bfe633a01dc99e84888176c04a75425dfb2
Gastón Morales <55248854+gmorales254@users.noreply.github.com>
2024-12-14 02:31:26 -0300

    fix typo

--- src/interfaces/goals.ts
+++ src/interfaces/goals.ts
@@ -95,7 +95,7 @@ export interface GoalNewDTO {
 	percentage_body_fat: number;
 	healthy_focus_description: string | null;
 	type: TypeGoals | string | null;
-	nutritionist_plans: NutritionistPlansServices | null;
+	nutritionist_plans: NutritionistPlansServices | string | null;
 	trainer_plans: CoachPlansServices | null | string;
 	amount_of_days: number;
 	initial_weight: number;
